85d4ad6d44a58612bc55cc032de2b80376256d50
Dalton Claybrook <dalton.claybrook@bottlerocketapps.com>
2015-05-19 18:09:16 -0500

    testing...

--- routes/example.js
+++ routes/example.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //var bodyParser = require('body-parser');
 //var jsonParser = bodyParser.json();
 
-router.post('/', jsonParser, function(req, res) {
+router.post('/', function(req, res) {
 
   //var name = req.body.item.message.from.mention_name;
   //res.send('Hey @' + name + ', I hope this works!');
