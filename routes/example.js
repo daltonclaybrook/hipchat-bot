70a2633e1dd422c5f01a37aab35f461004bf0b1a
Dalton Claybrook <dalton.claybrook@bottlerocketapps.com>
2015-05-19 17:27:09 -0500

    testing...

--- routes/example.js
+++ routes/example.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 router.post('/', function(req, res) {
 
-  var name = req.body.item.message.from.mention_name;
-  res.send('Hey @' + name + ', I hope this works!');
+  //var name = req.body.item.message.from.mention_name;
+  //res.send('Hey @' + name + ', I hope this works!');
+  res.send(JSON.stringify(req.body));
 });
 
 module.exports = router;
